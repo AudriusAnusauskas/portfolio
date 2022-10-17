65f82415941343adc1a9742e22ead1bbc963d4e5
AudriusAnusauskas <aaaudrius@gmail.com>
2022-10-17 20:16:20 +0300

    new project

---
+++ album-list/README.md
@@ -0,0 +1,4 @@
+# Album list
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/audriusanusauskas/pen/vYjMJVQ](https://codepen.io/audriusanusauskas/pen/vYjMJVQ).
+
---
+++ album-list/dist/script.js
@@ -0,0 +1,18 @@
+let artist = document.querySelector('input[name="artist"]');
+let album = document.querySelector('input[name="album"]');
+let year = document.querySelector('input[name="year"]')
+let button = document.querySelector('button');
+let list = document.querySelector('ul')
+
+function submit(){
+const albumItem = document.createElement('li');
+
+ albumItem.innerText = `${artist.value} ${album.value} ${year.value}`;  
+list.appendChild(albumItem)
+  artist.value = '';
+  album.value = '';
+  year.value = '';
+  
+}
+
+button.addEventListener('click', submit);
